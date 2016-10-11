cc5bc5b993ceba092a758cf579bd378d400bf7af
Emiliakazoo <emiliakazoo@gmail.com>
2016-10-10 23:43:47 -0700

    putting some more data on the page

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 (function() {
 
-var TestProto = {
-	questions: "What's the time?",
-	gameScore: function() {
-		//something here
-	}
-}
-
-var quiz = [
+var questions = [
 	{
 		"question": "What was Tommy’s girlfriend’s name?",
 		"answers": [
 			"Holly", "Gina", "Tina", "Selena"
 		],
-		"correctAnswer": "Gina"
+		"correctAnswer": 1
 	},
 	{
 		"question": "Where did Tommy work?",
 		"answers": [
 			"The Docks", "The Diner", "The Union Hall", "McDowell's"
 		],
-		"correctAnswer": "The Docks"	
+		"correctAnswer": 0	
 	},
 	{
 		"question": "What did Tommy’s girlfriend say didn’t make a difference?",
@@ -28,7 +21,7 @@ var quiz = [
 			"If he made it or not", "If we make it or not", "If they stayed cold or hot", "If they both smoked the pot"
 
 		],
-		"correctAnswer": "If we make it or not"	
+		"correctAnswer": 1	
 	},
 	{
 		"question": "How far were they?",
@@ -36,21 +29,21 @@ var quiz = [
 			"At the beginning", "At the end", "Halfway there", "At home, sleeping off last night’s party"
 
 		],
-		"correctAnswer": "Halfway there"	
+		"correctAnswer": 2	
 	},
 	{
 		"question": "What was it that Tommy’s girlfriend dreamed of?",
 		"answers": [
 			"Going to see Guns n Roses", "Getting a big fat raise", "Having a bunch of Tommy’s babies", "Running away"
 		],
-		"correctAnswer": "Running away"	
+		"correctAnswer": 3
 	},
 	{
 		"question": "Why was Tommy down on his luck?",
 		"answers": [
 			"His girl didn’t make much money", "His band was total crap", "The Union was on strike", "Praying didn’t seem to help much"
 		],
-		"correctAnswer": "The Union was on strike"	
+		"correctAnswer": 2	
 	},
 	{
 		"question": "What kind of hair did Tommy have?",
@@ -58,7 +51,7 @@ var quiz = [
 			"Buttrock hair", "Super-Teased Aqua-net", "Frosted and feathered", "Any and all of the above"
 
 		],
-		"correctAnswer": "Any and all of the above"	
+		"correctAnswer": 3
 	},
 	{
 		"question": "Did his girl wear tons of makeup and cheap perfume?",
@@ -66,55 +59,93 @@ var quiz = [
 			"Most likely", "Hell no", "What does it matter", "No, just lots of eyeliner and dark red lipstick"
 
 		],
-		"correctAnswer": "What does it matter"	
+		"correctAnswer": 2	
 	},
 	{
 		"question": "Was it tough?",
 		"answers": [
 			"So tough", "There wasn't enough", "It was more rough than anything", "No"
 		],
-		"correctAnswer": "So tough"	
+		"correctAnswer": 0
 	},
 	{
 		"question": "The first time you saw the video, did you imagine Tommy looked like JBJ?",
 		"answers": [
 			"JBJ IS Tommy!", "What?  No!", "What? Yes!", "I thought that long-haired person in the video was Gina."
 		],
-		"correctAnswer": "JBJ IS Tommy!"	
+		"correctAnswer": 0	
 	}, 
 
 ];
-
-var test = Object.create(TestProto);
-
-test.questions = quiz;
 
 
 //------------------------functions
 
 var startQuiz = $("form#quizStart");
 var showQuestions = $(".questions");
-var showFeedback = $(".rightOrWrong");
-var choice = $(".answers input[type=radio][name=answer]");
 var showCorrectAnswer = $(".wrongAnswer");
+var questionNumber = $(".questionNumber");
 var next = $(".next");
+var counter = 0;
+var numRight = 0;
+var numWrong = 0;
+$(".totalQuestions").text(questions.length);
+
+
+$("h3").text(questions[0].question);
+
+for(var i = 0; i < questions[0].answers.length; i+=1){
+        $(".answers").append("<li>" + questions[0].answers[i] + "</li>");
+}
+  counter += 1;
 
 startQuiz.on("submit", function(e){
 	e.preventDefault();
 	$(this).hide();
-	showQuestions.show();
+	$(".questions").show();
+	$(".status p").show();
+	questionNumber.text(counter);
 });
 
-	choice.change(function() {
-	    next.show();
-	    if (this.value == 'On the Docks') {
-	        showFeedback.prepend("<h2>Correct!</h2>");
-	    }
-	    else {
-	        showFeedback.prepend("<h2>WRONG!</h2>");
-	        showCorrectAnswer.show();
-	    }
-	    choice.prop("disabled", true);
-	});
+
+
+//____________________________________________________BEGIN event lister to start app
+next.on("click", function() {
+	$("h3").empty();
+	$(".answers").empty();
+	$(".rightOrWrong h2").empty();
+	$(".rightOrWrong p").empty();
+	$("h3").text(questions[counter].question);
+	$(this).hide();
+
+	for(var i = 0; i < questions[counter - 1].answers.length; i+=1){
+	        $(".answers").append("<li>" + questions[counter].answers[i] + "</li>");
+	}
+	  counter += 1;
+	  questionNumber.text(counter);
+});
+//____________________________________________________END event lister to start app
+
+
+
+//----------------------------------------------------BEGIN event listener to evaluate choice
+$("ul").on("click", "li", function() {
+	if($(this).index() == questions[counter - 1].correctAnswer){
+    	$(".rightOrWrong").prepend("<h2>Correct!</h2>");
+    	numRight += 1;
+    	$(".right").text(numRight);
+    }
+    else {
+    	var nums = questions[counter - 1].correctAnswer;
+    	$(".rightOrWrong").prepend("<h2>Wrong!!</h2> <p>The correct answer is " + questions[counter-1].answers[nums] + ".</p>");
+    	numWrong += 1;
+    	$(".wrong").text(numWrong);
+	}
+	next.show();
+
+})
+//-----------------------------------------------------------END evaluate choice
+
+//-----------------------------------------------------------BEGIN compare
 
 })();
